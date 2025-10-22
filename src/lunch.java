dd08a5938531320e3e717a064c2f91eace4abc44
yunkiseon <yks97016033@gmail.com>
2025-10-22 13:11:19 +0900

    lunch 추천

---
+++ src/lunch.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class lunch {
+    public static void main(String[] args) {
+        System.out.println("배고파요");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("메뉴추천");
+        String lunch = sc.nextLine();
+        System.out.println(lunch);
+    }
+}
